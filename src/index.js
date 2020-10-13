8f10aee7508e151bafe76daa8ad65b0bf18f88ab
Snake01 <vanya.kirdun@gmail.com>
2020-10-13 22:50:19 +0300

    init: compleate tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let mass=[];
+    for(let x = 0; x < bracketsConfig.length; x++){
+        mass.push(bracketsConfig[x].join(''));
+    }
+    for (let x = 0; x < mass.length; x++) {
+        if (str.includes(mass[x])) {
+            str = str.replace(mass[x], '');
+            x = -1;
+        }
+    }
+    return (str) ? false : true;
 }
